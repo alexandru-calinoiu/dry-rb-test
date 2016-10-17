7c214c05f68b3c9bb733de4fb2927ec74acdf82f
Calin <calinoiu.alexandru@agilefreaks.com>
2016-10-17 19:08:33 +0300

    Bundle install works now

---
+++ lib/dry/rb/test.rb
@@ -0,0 +1,9 @@
+require "dry/rb/test/version"
+
+module Dry
+  module Rb
+    module Test
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/dry/rb/test/version.rb
@@ -0,0 +1,7 @@
+module Dry
+  module Rb
+    module Test
+      VERSION = "0.1.0"
+    end
+  end
+end
